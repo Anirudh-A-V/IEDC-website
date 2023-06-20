65fb9cae337055a11c5f39f2aebc197fe2e278a7
Abhishek A S <98143098+ABHISHEK-A-S@users.noreply.github.com>
2023-06-20 11:12:45 +0530

    Changed One

--- src/App.js
+++ src/App.js
@@ -23,28 +23,39 @@ import Enovus from './views/Enovus';
 
 const App = () => {
 
-  const childRef = useRef();
-  let location = useLocation();
+    const childRef = useRef();
+    let location = useLocation();
 
-  useEffect(() => {
-    // const page = location.pathname;
-    document.body.classList.add('is-loaded')
-    childRef.current.init();
-    // trackPage(page);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location]);
+    useEffect(() => {
+        // const page = location.pathname;
+        document.body.classList.add('is-loaded')
+        childRef.current.init();
+        // trackPage(page);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location]);
 
-  return (
-    <ScrollReveal
-      ref={childRef}
-      children={() => (
-        <Switch>
-          <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
-          <AppRoute exact path="/ignites" component={Ignites} layout={LayoutIgnites} />
-          <AppRoute exact path="/enovus" component={Enovus} />
-        </Switch>
-      )} />
-  );
+    return ( <
+        ScrollReveal ref = { childRef }
+        children = {
+            () => ( <
+                Switch >
+                <
+                AppRoute exact path = "/"
+                component = { Home }
+                layout = { LayoutDefault }
+                /> <
+                AppRoute exact path = "/ignites"
+                component = { Ignites }
+                layout = { LayoutIgnites }
+                /> <
+                AppRoute exact path = "/enovus"
+                component = { Enovus }
+                /> <
+                /Switch>
+            )
+        }
+        />
+    );
 }
 
 export default App;
